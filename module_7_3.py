1f060904caf0a7b7678fbb50633a95ec0593be5c
Not_Enough <superlexlive@gmail.com>
2024-11-30 14:10:20 +0300

    module_7_3.py

--- module_7_3.py
+++ module_7_3.py
@@ -2,41 +2,60 @@ import string
 
 class WordsFinder:
     def __init__(self, *file_names):
+        """
+        Конструктор, принимает названия файлов и сохраняет их в атрибут file_names.
+        """
         self.file_names = list(file_names)
 
     def get_all_words(self):
+        """
+        Читает слова из каждого файла, возвращая словарь вида:
+        {'file1.txt': ['word1', 'word2'], 'file2.txt': ['word3', 'word4']}
+        """
         all_words = {}
         for file_name in self.file_names:
-            with open(file_name, 'r', encoding='utf-8') as file:
-                content = file.read()
-            
-            content = content.lower()
-            content = ''.join(char for char in content 
-                  if char.isalnum() or char.isspace() or char == "'")
-            
-            words = content.split()
-            all_words[file_name] = words
+            try:
+                with open(file_name, 'r', encoding='utf-8') as file:
+                    text = file.read().lower()
+                    for punct in [',', '.', '=', '!', '?', ';', ':', ' - ']:
+                        text = text.replace(punct, ' ')
+                    words = text.split()
+                    all_words[file_name] = words
+            except FileNotFoundError:
+                all_words[file_name] = []
         return all_words
 
     def find(self, word):
+        """
+        Ищет слово в каждом файле, возвращая словарь:
+        {'file1.txt': позиция, ...}
+        Если слово не найдено, позиция равна -1.
+        """
+        word = word.lower()
         all_words = self.get_all_words()
         result = {}
-        for file_name, words in all_words.items():
+        for name, words in all_words.items():
             try:
-                index = words.index(word)
-                result[file_name] = index
+                position = words.index(word) + 1
             except ValueError:
-                pass
+                position = -1
+            result[name] = position
         return result
 
     def count(self, word):
+        """
+        Считает количество вхождений слова в каждом файле, возвращая словарь:
+        {'file1.txt': количество, ...}
+        """
+        word = word.lower()
         all_words = self.get_all_words()
         result = {}
-        for file_name, words in all_words.items():
-            result[file_name] = words.count(word)
+        for name, words in all_words.items():
+            result[name] = words.count(word)
         return result
 
-finder2 = WordsFinder('test_file.txt')
-print(finder2.get_all_words())
-print(finder2.find('TEXT'))
-print(finder2.count('teXT'))
+finder = WordsFinder('test_file.txt')
+
+print(finder.get_all_words())
+print(finder.find('TEXT'))
+print(finder.count('teXT'))
